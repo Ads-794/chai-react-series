f1f216aa53a91eef68b79cdba21432d2db700c7c
Aditya Dixit <aadityaadixit@gmail.com>
2024-08-16 17:10:54 +0530

    Currency Convertor

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => {
+        let countryCode = currency
+        setData(res.rates)
+      })
+      .catch((error) => console.error("Error:", error))
+
+    // setData(res.rates)
+  }, [currency])
+  return data
+}
+
+export default useCurrencyInfo
